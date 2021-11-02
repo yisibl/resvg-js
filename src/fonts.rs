19518e214a1ddbac6445b93b60369dd6aadcf74b
yisibl <percyley@qq.com>
2021-11-02 19:57:31 +0800

    chore: upgrade Rust Edition to 2021

--- src/fonts.rs
+++ src/fonts.rs
@@ -3,7 +3,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 use crate::options::*;
-use log::{debug, error, warn};
+use log::{debug, warn};
 use usvg::fontdb::Database;
 
 /// Loads fonts.
