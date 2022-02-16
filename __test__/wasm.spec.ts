1786258d0844d5bf1adac729b952519d148f2f96
yisibl <percyley@qq.com>
2022-02-16 19:07:05 +0800

    test: add render() crop option case

--- __test__/wasm.spec.ts
+++ __test__/wasm.spec.ts
@@ -82,6 +82,25 @@ test('Set the background without alpha by hsla()', async (t) => {
   t.is(result.hasAlpha(), false)
 })
 
+test('should generate a 80x80 png and opaque', async (t) => {
+  const svg = `<svg width="200px" height="200px" viewBox="0 0 200 200" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <rect fill="green" x="0" y="0" width="100" height="100"></rect>
+  </svg>`
+  const pngData = render(svg, {
+    crop: {
+      left: 20,
+      top: 20,
+      right: 100,
+      bottom: 100,
+    },
+  })
+  const result = await jimp.read(Buffer.from(pngData))
+
+  t.is(result.getWidth(), 100 - 20)
+  t.is(result.getHeight(), 100 - 20)
+  t.is(result.hasAlpha(), false)
+})
+
 // throws
 test('should throw because invalid SVG (blank string)', (t) => {
   const error = t.throws(
