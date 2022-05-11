01c454c17efe343563a6a504e5d32f59be235edd
Zimond <daizhuoxian@gmail.com>
2022-05-11 11:25:13 +0800

    feat: return undefined if bbox is invalid

--- wasm/index.js
+++ wasm/index.js
@@ -2,24 +2,19 @@ var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
 var __export = (target, all) => {
   for (var name in all)
     __defProp(target, name, { get: all[name], enumerable: true });
 };
-var __reExport = (target, module2, copyDefault, desc) => {
-  if (module2 && typeof module2 === "object" || typeof module2 === "function") {
-    for (let key of __getOwnPropNames(module2))
-      if (!__hasOwnProp.call(target, key) && (copyDefault || key !== "default"))
-        __defProp(target, key, { get: () => module2[key], enumerable: !(desc = __getOwnPropDesc(module2, key)) || desc.enumerable });
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
   }
-  return target;
+  return to;
 };
-var __toCommonJS = /* @__PURE__ */ ((cache) => {
-  return (module2, temp) => {
-    return cache && cache.get(module2) || (temp = __reExport(__markAsModule({}), module2, 1), cache && cache.set(module2, temp), temp);
-  };
-})(typeof WeakMap !== "undefined" ? /* @__PURE__ */ new WeakMap() : 0);
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
 // wasm-binding.ts
 var wasm_binding_exports = {};
@@ -27,6 +22,7 @@ __export(wasm_binding_exports, {
   Resvg: () => Resvg2,
   initWasm: () => initWasm
 });
+module.exports = __toCommonJS(wasm_binding_exports);
 
 // wasm/dist/index.js
 var wasm;
@@ -389,4 +385,3 @@ var Resvg2 = class extends Resvg {
     super(svg, JSON.stringify(options));
   }
 };
-module.exports = __toCommonJS(wasm_binding_exports);
