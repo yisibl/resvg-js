f78832b40709cb4b2d02be989bb633992698c471
yisibl <percyley@qq.com>
2022-05-21 22:32:52 +0800

    style: rust indent changed from 2 to 4 spaces

--- src/error.rs
+++ src/error.rs
@@ -6,32 +6,32 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-  #[error(transparent)]
-  SVG(#[from] svgtypes::Error),
-  #[error(transparent)]
-  USvg(#[from] usvg::Error),
-  #[error(transparent)]
-  Encoding(#[from] png::EncodingError),
-  #[error(transparent)]
-  Utf8(#[from] std::string::FromUtf8Error),
-  #[error("Target size is zero (please do not set the width/height/zoom options to 0)")]
-  ZeroSized,
-  #[error("Input must be string or Uint8Array")]
-  InvalidInput,
-  #[error("Unrecognized image buffer")]
-  UnrecognizedBuffer,
+    #[error(transparent)]
+    SVG(#[from] svgtypes::Error),
+    #[error(transparent)]
+    USvg(#[from] usvg::Error),
+    #[error(transparent)]
+    Encoding(#[from] png::EncodingError),
+    #[error(transparent)]
+    Utf8(#[from] std::string::FromUtf8Error),
+    #[error("Target size is zero (please do not set the width/height/zoom options to 0)")]
+    ZeroSized,
+    #[error("Input must be string or Uint8Array")]
+    InvalidInput,
+    #[error("Unrecognized image buffer")]
+    UnrecognizedBuffer,
 }
 
 #[cfg(not(target_arch = "wasm32"))]
 impl From<Error> for napi::Error {
-  fn from(e: Error) -> Self {
-    napi::Error::from_reason(format!("{}", e))
-  }
+    fn from(e: Error) -> Self {
+        napi::Error::from_reason(format!("{}", e))
+    }
 }
 
 #[cfg(target_arch = "wasm32")]
 impl From<Error> for js_sys::Error {
-  fn from(e: Error) -> Self {
-    js_sys::Error::new(&format!("{}", e))
-  }
+    fn from(e: Error) -> Self {
+        js_sys::Error::new(&format!("{}", e))
+    }
 }
--- src/fonts.rs
+++ src/fonts.rs
@@ -14,70 +14,70 @@ use usvg::fontdb::Database;
 /// Loads fonts.
 #[cfg(not(target_arch = "wasm32"))]
 pub fn load_fonts(font_options: &JsFontOptions) -> Database {
-  // Create a new font database
-  let mut fontdb = Database::new();
-  let now = std::time::Instant::now();
+    // Create a new font database
+    let mut fontdb = Database::new();
+    let now = std::time::Instant::now();
 
-  // 加载系统字体
-  // https://github.com/RazrFalcon/fontdb/blob/052d74b9eb45f2c4f446846a53f33bd965e2662d/src/lib.rs#L261
-  if font_options.load_system_fonts {
-    fontdb.load_system_fonts();
-  }
-
-  // 加载指定路径的字体
-  for path in &font_options.font_files {
-    if let Err(e) = fontdb.load_font_file(path) {
-      warn!("Failed to load '{}' cause {}.", path, e);
+    // 加载系统字体
+    // https://github.com/RazrFalcon/fontdb/blob/052d74b9eb45f2c4f446846a53f33bd965e2662d/src/lib.rs#L261
+    if font_options.load_system_fonts {
+        fontdb.load_system_fonts();
     }
-  }
 
-  // Load font directories
-  for path in &font_options.font_dirs {
-    fontdb.load_fonts_dir(path);
-  }
-
-  // Set generic font families
-  // - `serif` - Times New Roman
-  // - `sans-serif` - Arial
-  // - `cursive` - Comic Sans MS
-  // - `fantasy` - Impact (Papyrus on macOS)
-  // - `monospace` - Courier New
-  fontdb.set_serif_family(&font_options.serif_family);
-  fontdb.set_sans_serif_family(&font_options.sans_serif_family);
-  fontdb.set_cursive_family(&font_options.cursive_family);
-  fontdb.set_fantasy_family(&font_options.fantasy_family);
-  fontdb.set_monospace_family(&font_options.monospace_family);
-  debug!(
-    "Loaded {} font faces in {}ms.",
-    fontdb.len(),
-    now.elapsed().as_micros() as f64 / 1000.0
-  );
-
-  // 查找指定字体的路径
-  let font_family: &str = &font_options.default_font_family;
-  let query = fontdb::Query {
-    families: &[fontdb::Family::Name(font_family)],
-    ..fontdb::Query::default()
-  };
-
-  let now = std::time::Instant::now();
-  // 当前使用的字体是否存在
-  match fontdb.query(&query) {
-    Some(id) => {
-      let (src, index) = fontdb.face_source(id).unwrap();
-      if let fontdb::Source::File(ref path) = &src {
-        debug!(
-          "Font '{}':{} found in {}ms.",
-          path.display(),
-          index,
-          now.elapsed().as_micros() as f64 / 1000.0
-        );
-      }
+    // 加载指定路径的字体
+    for path in &font_options.font_files {
+        if let Err(e) = fontdb.load_font_file(path) {
+            warn!("Failed to load '{}' cause {}.", path, e);
+        }
     }
-    None => {
-      warn!("Warning: The default font '{}' not found.", font_family);
-    }
-  }
 
-  fontdb
+    // Load font directories
+    for path in &font_options.font_dirs {
+        fontdb.load_fonts_dir(path);
+    }
+
+    // Set generic font families
+    // - `serif` - Times New Roman
+    // - `sans-serif` - Arial
+    // - `cursive` - Comic Sans MS
+    // - `fantasy` - Impact (Papyrus on macOS)
+    // - `monospace` - Courier New
+    fontdb.set_serif_family(&font_options.serif_family);
+    fontdb.set_sans_serif_family(&font_options.sans_serif_family);
+    fontdb.set_cursive_family(&font_options.cursive_family);
+    fontdb.set_fantasy_family(&font_options.fantasy_family);
+    fontdb.set_monospace_family(&font_options.monospace_family);
+    debug!(
+        "Loaded {} font faces in {}ms.",
+        fontdb.len(),
+        now.elapsed().as_micros() as f64 / 1000.0
+    );
+
+    // 查找指定字体的路径
+    let font_family: &str = &font_options.default_font_family;
+    let query = fontdb::Query {
+        families: &[fontdb::Family::Name(font_family)],
+        ..fontdb::Query::default()
+    };
+
+    let now = std::time::Instant::now();
+    // 当前使用的字体是否存在
+    match fontdb.query(&query) {
+        Some(id) => {
+            let (src, index) = fontdb.face_source(id).unwrap();
+            if let fontdb::Source::File(ref path) = &src {
+                debug!(
+                    "Font '{}':{} found in {}ms.",
+                    path.display(),
+                    index,
+                    now.elapsed().as_micros() as f64 / 1000.0
+                );
+            }
+        }
+        None => {
+            warn!("Warning: The default font '{}' not found.", font_family);
+        }
+    }
+
+    fontdb
 }
