f3c5ca0f69d2771ceea8ec21c8dfccdec8ca9a16
一丝 <percyley@qq.com>
2023-02-07 15:19:53 +0800

    chore: remove bench-related dependencies
    Also disable skr-canvas in the bench

--- benchmark/bench.ts
+++ benchmark/bench.ts
@@ -1,7 +1,7 @@
 import { promises as fs } from 'fs'
 import { join } from 'path'
 
-import { createCanvas, Image } from '@napi-rs/canvas'
+// import { createCanvas, Image } from '@napi-rs/canvas'
 import b from 'benny'
 import sharp from 'sharp'
 import svg2img from 'svg2img'
@@ -37,25 +37,25 @@ async function run() {
     }),
 
     // test from https://github.com/Brooooooklyn/canvas/blob/main/example/resize-svg.js
-    b.add('skr-canvas(Rust)', () => {
-      const image = new Image()
-      image.src = svg1
+    // b.add('skr-canvas(Rust)', () => {
+    //   const image = new Image()
+    //   image.src = svg1
 
-      const w = 1200
-      const h = 623
+    //   const w = 1200
+    //   const h = 623
 
-      // resize SVG
-      image.width = w
-      image.height = h
+    //   // resize SVG
+    //   image.width = w
+    //   image.height = h
 
-      // create a canvas of the same size as the image
-      const canvas = createCanvas(w, h)
-      const ctx = canvas.getContext('2d')
+    //   // create a canvas of the same size as the image
+    //   const canvas = createCanvas(w, h)
+    //   const ctx = canvas.getContext('2d')
 
-      // fill the canvas with the image
-      ctx.drawImage(image, 0, 0)
-      canvas.toBuffer('image/png')
-    }),
+    //   // fill the canvas with the image
+    //   ctx.drawImage(image, 0, 0)
+    //   canvas.toBuffer('image/png')
+    // }),
 
     b.add('svg2img(canvg + node-canvas)', () => {
       svg2img(svg1.toString('utf8'), { width: 1200, height: 623 }, function (_error, _buffer) {})
@@ -93,25 +93,25 @@ async function run() {
     }),
 
     // test from https://github.com/Brooooooklyn/canvas/blob/main/example/resize-svg.js
-    b.add('skr-canvas(Rust)', () => {
-      const image = new Image()
-      image.src = icon
+    // b.add('skr-canvas(Rust)', () => {
+    //   const image = new Image()
+    //   image.src = icon
 
-      const w = 386
-      const h = 386
+    //   const w = 386
+    //   const h = 386
 
-      // resize SVG
-      image.width = w
-      image.height = h
+    //   // resize SVG
+    //   image.width = w
+    //   image.height = h
 
-      // create a canvas of the same size as the image
-      const canvas = createCanvas(w, h)
-      const ctx = canvas.getContext('2d')
+    //   // create a canvas of the same size as the image
+    //   const canvas = createCanvas(w, h)
+    //   const ctx = canvas.getContext('2d')
 
-      // fill the canvas with the image
-      ctx.drawImage(image, 0, 0)
-      canvas.toBuffer('image/png')
-    }),
+    //   // fill the canvas with the image
+    //   ctx.drawImage(image, 0, 0)
+    //   canvas.toBuffer('image/png')
+    // }),
 
     b.add('svg2img(canvg + node-canvas)', () => {
       svg2img(icon.toString('utf8'), { width: 386, height: 386 }, function (_error, _buffer) {})
@@ -139,25 +139,25 @@ async function run() {
       await sharp(tiger).toBuffer()
     }),
 
-    b.add('skr-canvas(Rust)', () => {
-      const image = new Image()
-      image.src = tiger
+    // b.add('skr-canvas(Rust)', () => {
+    //   const image = new Image()
+    //   image.src = tiger
 
-      const w = 900
-      const h = 900
+    //   const w = 900
+    //   const h = 900
 
-      // resize SVG
-      image.width = w
-      image.height = h
+    //   // resize SVG
+    //   image.width = w
+    //   image.height = h
 
-      // create a canvas of the same size as the image
-      const canvas = createCanvas(w, h)
-      const ctx = canvas.getContext('2d')
+    //   // create a canvas of the same size as the image
+    //   const canvas = createCanvas(w, h)
+    //   const ctx = canvas.getContext('2d')
 
-      // fill the canvas with the image
-      ctx.drawImage(image, 0, 0)
-      canvas.toBuffer('image/png')
-    }),
+    //   // fill the canvas with the image
+    //   ctx.drawImage(image, 0, 0)
+    //   canvas.toBuffer('image/png')
+    // }),
 
     b.add('svg2img(canvg + node-canvas)', () => {
       svg2img(tiger.toString('utf8'), { width: 900, height: 900 }, function (_error, _buffer) {})
