eb26bf4ad8ab88c8bd032a01cf93a7152f7e7741
一丝 <percyley@qq.com>
2023-09-20 21:02:18 +0800

    test: using simple SVG files to make testing faster
    text.svg => bbox.svg

--- __test__/wasm.spec.ts
+++ __test__/wasm.spec.ts
@@ -101,14 +101,14 @@ test('buffer input', async (t) => {
 })
 
 test('fit to width', async (t) => {
-  const filePath = '../example/text.svg'
+  const filePath = '../example/bbox.svg'
   const svg = await fs.readFile(join(__dirname, filePath))
   const svgString = svg.toString('utf-8')
   const opts = {
     background: '#eeebe6',
     fitTo: {
       mode: 'width',
-      value: 1200,
+      value: 800,
     },
   }
   const resvg = new Resvg(svgString, opts)
@@ -116,12 +116,12 @@ test('fit to width', async (t) => {
   const pngBuffer = pngData.asPng()
   const result = await jimp.read(Buffer.from(pngBuffer))
 
-  t.is(result.getWidth(), 1200)
-  t.is(result.getHeight(), 623)
+  t.is(result.getWidth(), 800)
+  t.is(result.getHeight(), 1156)
 })
 
 test('fit to height', async (t) => {
-  const filePath = '../example/text.svg'
+  const filePath = '../example/bbox.svg'
   const svg = await fs.readFile(join(__dirname, filePath))
   const svgString = svg.toString('utf-8')
   const opts = {
@@ -136,23 +136,23 @@ test('fit to height', async (t) => {
   const pngBuffer = pngData.asPng()
   const result = await jimp.read(Buffer.from(pngBuffer))
 
-  t.is(result.getWidth(), 1003)
+  t.is(result.getWidth(), 360)
   t.is(result.getHeight(), 520)
 })
 
 test('Get SVG original size', async (t) => {
-  const filePath = '../example/text.svg'
+  const filePath = '../example/bbox.svg'
   const svg = await fs.readFile(join(__dirname, filePath))
   const resvg = new Resvg(svg, {
     background: '#eeebe6',
     fitTo: {
       mode: 'width',
-      value: 1200, // The original size is not affected by the fitTo parameter
+      value: 600, // The original size is not affected by the fitTo parameter
     },
   })
 
-  t.is(resvg.width, 1324)
-  t.is(resvg.height, 687)
+  t.is(resvg.width, 180)
+  t.is(resvg.height, 260)
 })
 
 test('SVG size must be rounded to an integer', (t) => {
