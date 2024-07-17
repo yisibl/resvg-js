3278ef47c66880e8aa19066ff8c54cffcdbe96b6
Zhengqbbb <1074059947@qq.com>
2024-07-18 01:01:41 +0800

    chore(cli): ensure CLI dev and prepublish script same

--- bundle-prepublish.js
+++ bundle-prepublish.js
@@ -1,20 +1,12 @@
 // @ts-check
 const { buildSync } = require('esbuild')
 
+require('./bundle-cli')
+
 buildSync({
   entryPoints: ['js-binding.js'],
   allowOverwrite: true,
   outfile: 'js-binding.js',
   logLevel: 'info',
   minify: true,
-})
-
-buildSync({
-  entryPoints: ['cli/cli.js'],
-  outfile: 'cli.js',
-  logLevel: 'info',
-  minify: false,
-  bundle: true,
-  platform: 'node',
-  external: ['./index'],
 })
